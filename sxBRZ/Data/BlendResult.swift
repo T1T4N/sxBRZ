b32a39fba3984637778f07386e79396889d03c76
Robert Armenski <r.armenski@gmail.com>
2016-05-08 06:29:34 +0200

    Implement CustomStringConvertible protocol

--- sxBRZ/Data/BlendResult.swift
+++ sxBRZ/Data/BlendResult.swift
@@ -7,9 +7,15 @@
 
 import Foundation
 
-struct BlendResult
+struct BlendResult: CustomStringConvertible
 {
     var
     /**/blend_f:BlendType = BlendType.BLEND_NONE, blend_g:BlendType = BlendType.BLEND_NONE,
     /**/blend_j:BlendType = BlendType.BLEND_NONE, blend_k:BlendType = BlendType.BLEND_NONE
+    
+    var description: String {
+        get {
+            return String(format: "%d %d %d %d", self.blend_f.rawValue, self.blend_g.rawValue, self.blend_j.rawValue, self.blend_k.rawValue)
+        }
+    }
 };
--- sxBRZ/Data/RotationDegree.swift
+++ sxBRZ/Data/RotationDegree.swift
@@ -7,9 +7,18 @@
 
 import Foundation
 
-enum RotationDegree : Int {
-    case ROT_0
-    case ROT_90
-    case ROT_180
-    case ROT_270
+enum RotationDegree : Int, CustomStringConvertible {
+    case ROT_0 = 0
+    case ROT_90 = 1
+    case ROT_180 = 2
+    case ROT_270 = 3
+    
+    var description : String {
+        switch self {
+        case .ROT_0: return "ROT_0";
+        case .ROT_90: return "ROT_90";
+        case .ROT_180: return "ROT_180";
+        case .ROT_270: return "ROT_270";
+        }
+    }
 }
