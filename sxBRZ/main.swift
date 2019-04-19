c5da62d7429f849a8935d9a5de9ea950cfc329cf
T1T4N <r.armenski@gmail.com>
2019-04-19 12:02:47 +0200

    Use guard statements

--- sxBRZ/main.swift
+++ sxBRZ/main.swift
@@ -8,17 +8,9 @@
 import Foundation
 import AppKit
 
-func validScale(_ scale: String) -> Bool {
+func isValid(scale: String) -> Bool {
     switch scale {
-    case "2":
-        return true
-    case "3":
-        return true
-    case "4":
-        return true
-    case "5":
-        return true
-    case "6":
+    case "2", "3", "4", "5", "6":
         return true
     default:
         return false
@@ -35,64 +27,67 @@ let inPath = CommandLine.arguments[1]
 let requestedScale = CommandLine.arguments[2]
 let outPath = CommandLine.arguments[3]
 
-if !validScale(requestedScale) {
-    print("Scale must be 2, 3, 4, 5 or 6")
-    exit(EXIT_FAILURE)
+guard isValid(scale: requestedScale),
+    let scaleFactor = Int(requestedScale) else {
+        print("Scale must be 2, 3, 4, 5 or 6")
+        exit(EXIT_FAILURE)
 }
 
-let scaleFactor = Int(requestedScale)!
-if let image = NSImage(contentsOf: URL(fileURLWithPath: inPath)) {
-    print("Loading image from path: \(inPath)")
-    if let cgRef = image.cgImage(forProposedRect: nil, context: nil, hints: nil) {
-        let width = cgRef.width
-        let height = cgRef.height
+print("Loading image from path: \(inPath)")
+guard let image = NSImage(contentsOf: URL(fileURLWithPath: inPath)),
+    let cgRef = image.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+        print("Failed to load image")
+        exit(EXIT_FAILURE)
+}
 
-        let outWidth = scaleFactor * width
-        let outHeight = scaleFactor * height
+let width = cgRef.width
+let height = cgRef.height
 
-        var p_raw = getImageData(cgRef)
-        let rawpt = UnsafeMutablePointer<UInt32>(mutating: p_raw)
+let outWidth = scaleFactor * width
+let outHeight = scaleFactor * height
 
-        var p_output = [UInt32](repeating: 0, count: scaleFactor * scaleFactor * height * width)
-        var outpt = UnsafeMutablePointer<UInt32>(mutating: p_output)
+var rawData = getImageData(cgRef)
+let rawpt = UnsafeMutablePointer<UInt32>(mutating: rawData)
 
-        // p_fin contains the data converted to ARGB
-        var p_fin = [UInt32](repeating: 0, count: scaleFactor * scaleFactor * height * width)
-        let finpt = UnsafeMutablePointer<UInt32>(mutating: p_fin)
+var p_output = [UInt32](repeating: 0, count: scaleFactor * scaleFactor * height * width)
+var outpt = UnsafeMutablePointer<UInt32>(mutating: p_output)
 
-        var cfg = ScalerCfg()
-        scale(UInt(scaleFactor), rawpt, &outpt, width, height, ColorFormat.argb, cfg)
-        //        scale(UInt(scaleFactor), p_raw, &p_output, width, height, ColorFormat.ARGB, cfg)
-        //        xBRZC.scale(scaleFactor, source: rawpt, target: outpt, width: Int32(width), height: Int32(height), hasAlpha: true)
+// p_fin contains the data converted to ARGB
+var p_fin = [UInt32](repeating: 0, count: scaleFactor * scaleFactor * height * width)
+let finpt = UnsafeMutablePointer<UInt32>(mutating: p_fin)
 
-        // Convert RGBA to ARGB
-        outpt.withMemoryRebound(to: UInt8.self, capacity: MemoryLayout.size(ofValue: p_output)) { convpt in
-            for y in 0 ..< outWidth {
-                for x in 0 ..< outHeight {
-                    let offset = 4 * (x * outWidth + y)
+var cfg = ScalerCfg()
+scale(UInt(scaleFactor), rawpt, &outpt, width, height, ColorFormat.argb, cfg)
+//scale(UInt(scaleFactor), p_raw, &p_output, width, height, ColorFormat.ARGB, cfg)
+//xBRZC.scale(scaleFactor, source: rawpt, target: outpt, width: Int32(width), height: Int32(height), hasAlpha: true)
 
-                    let a: UInt32 = UInt32(convpt[offset])
-                    let r: UInt32 = UInt32(convpt[offset + 1])
-                    let g: UInt32 = UInt32(convpt[offset + 2])
-                    let b: UInt32 = UInt32(convpt[offset + 3])
-
-                    finpt[x * outWidth + y] = (a << 24) | (r << 16) | (g << 8) | b
-                }
-            }
+// Convert RGBA to ARGB
+outpt.withMemoryRebound(to: UInt8.self, capacity: MemoryLayout.size(ofValue: p_output)) { convpt in
+    for y in 0 ..< outWidth {
+        for x in 0 ..< outHeight {
+            let offset = 4 * (x * outWidth + y)
+            
+            let a: UInt32 = UInt32(convpt[offset])
+            let r: UInt32 = UInt32(convpt[offset + 1])
+            let g: UInt32 = UInt32(convpt[offset + 2])
+            let b: UInt32 = UInt32(convpt[offset + 3])
+            
+            finpt[x * outWidth + y] = (a << 24) | (r << 16) | (g << 8) | b
         }
-
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)
-        let modCont = CGContext(data: UnsafeMutableRawPointer(finpt), width: outWidth, height: outHeight, bitsPerComponent: 8, bytesPerRow: 4 * outWidth, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
-        let modRef = modCont?.makeImage()
-
-        let imgRep = NSBitmapImageRep(cgImage: modRef!)
-        var data = imgRep.representation(using: NSBitmapImageRep.FileType.bmp, properties: [:])
-        ((try? data?.write(to: URL(fileURLWithPath: outPath), options: [])) as ()??)
-        print("Image successfully scaled")
     }
+}
 
-} else {
-    print("Missing image at: \(inPath)")
-    exit(EXIT_FAILURE)
+let colorSpace = CGColorSpaceCreateDeviceRGB()
+let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)
+let modCont = CGContext(data: UnsafeMutableRawPointer(finpt), width: outWidth, height: outHeight, bitsPerComponent: 8, bytesPerRow: 4 * outWidth, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+let modRef = modCont?.makeImage()
+
+let imgRep = NSBitmapImageRep(cgImage: modRef!)
+var data = imgRep.representation(using: NSBitmapImageRep.FileType.bmp, properties: [:])
+
+do {
+    try data?.write(to: URL(fileURLWithPath: outPath), options: [])
+    print("Image successfully scaled")
+} catch {
+    print("Failed to write output data: \(error)")
 }
