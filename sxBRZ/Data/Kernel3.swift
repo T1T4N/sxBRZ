b1dd042acf62af57556da632118c8af08886b145
T1T4N <r.armenski@gmail.com>
2019-04-19 12:58:05 +0200

    Refactor kernels

---
+++ sxBRZ/Data/Kernel3.swift
@@ -0,0 +1,25 @@
+//
+//  Kernel3.swift
+//  sxBRZ
+//
+//  Created by Robert Armenski on 19.04.19.
+//  Copyright © 2019 TitanTech. All rights reserved.
+//
+
+import Foundation
+
+struct Kernel_3x3 {
+    let
+    a: UInt32, b: UInt32, c: UInt32,
+    d: UInt32, e: UInt32, f: UInt32,
+    g: UInt32, h: UInt32, i: UInt32
+}
+
+extension Kernel_3x3: CustomStringConvertible {
+    var description: String {
+        return String(format: "%d %d %d %d %d %d %d %d %d",
+                      self.a, self.b, self.c,
+                      self.d, self.e, self.f,
+                      self.g, self.h, self.i)
+    }
+}
---
+++ sxBRZ/Data/Kernel4.swift
@@ -0,0 +1,27 @@
+//
+//  Kernel.swift
+//  sxBRZ
+//
+//  Created by T!T@N on 04.25.16.
+//
+
+import Foundation
+
+/// Kernel for the preprocessing step
+struct Kernel_4x4 {
+    let
+    a: UInt32, b: UInt32, c: UInt32, d: UInt32,
+    e: UInt32, f: UInt32, g: UInt32, h: UInt32,
+    i: UInt32, j: UInt32, k: UInt32, l: UInt32,
+    m: UInt32, n: UInt32, o: UInt32, p: UInt32
+}
+
+extension Kernel_4x4: CustomStringConvertible {
+    var description: String {
+        return String(format: "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+                      self.a, self.b, self.c, self.d,
+                      self.e, self.f, self.g, self.h,
+                      self.i, self.j, self.k, self.l,
+                      self.m, self.n, self.o, self.p)
+    }
+}
