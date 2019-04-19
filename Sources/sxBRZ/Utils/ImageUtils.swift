18ced855b6852aed0169b66a8e07c3b752decb2f
T1T4N <r.armenski@gmail.com>
2019-04-20 00:23:31 +0200

    Remove unused code

--- Sources/sxBRZ/Utils/ImageUtils.swift
+++ Sources/sxBRZ/Utils/ImageUtils.swift
@@ -28,62 +28,7 @@ private func _createARGBBitmapContext(_ imageRef: CGImage) -> CGContext! {
                      bitmapInfo: bitmapInfo.rawValue)!
 }
 
-func manipulatePixel(_ imageRef: CGImage) -> CGImage? {
-    let context = imageRef.createARGBBitmapContext()
-    let width = imageRef.width
-    let height = imageRef.height
-    let rect = CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height))
-    let bitmapBytesPerRow = Int(width) * 4
-
-    //Clear the context
-    context.clear(rect)
-
-    // Draw the image to the bitmap context. Once we draw, the memory
-    // allocated for the context for rendering will then contain the
-    // raw image data in the specified color space.
-    context.draw(imageRef, in: rect)
-
-    guard let data = context.data else { return nil }
-    defer { free(data) }
-
-    let dataType = data.assumingMemoryBound(to: UInt32.self)
-    // let dataType = UnsafeMutablePointer<UInt32>(data)
-    // let dataType = UnsafeMutablePointer<UInt8>(data)
-
-    for y in 0 ..< width {
-        for x in (height - 20) ..< height {
-            // If dataType == UInt32
-            let offset = (width * x) + y
-            // If dataType == UInt8
-            // let offset = 4*((width * x) + y)
-
-            let a: UInt32 = 255
-            let r: UInt32 = 0
-            let g: UInt32 = 255
-            let b: UInt32 = 0
-
-            // dataType[offset] = a
-            // dataType[offset + 1] = r
-            // dataType[offset + 2] = g
-            // dataType[offset + 3] = b
-
-            dataType[offset] = (b << 24) | (g << 16) | (r << 8) | a
-        }
-    }
-    let colorSpace = CGColorSpaceCreateDeviceRGB()
-    let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)
-    let contextRef = CGContext(data: data,
-                               width: width, height: height,
-                               bitsPerComponent: 8,
-                               bytesPerRow: bitmapBytesPerRow,
-                               space: colorSpace,
-                               bitmapInfo: bitmapInfo.rawValue)
-    let imageRef = contextRef?.makeImage()
-    // let imageRef = CGBitmapContextCreateImage(context)
-    return imageRef
-}
-
-func getPixelData(_ imageRef: CGImage) -> [UInt32] {
+private func getPixelData(_ imageRef: CGImage) -> [UInt32] {
     let context = imageRef.createARGBBitmapContext()
     let width = imageRef.width
     let height = imageRef.height
